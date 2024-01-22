ba0d0e972d4ceec326f86bb502f56475b89d6080
007 <dr007cc@gmail.com>
2024-01-23 01:16:06 +0330

    fix: update

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+
+SPACER="======================================"
+EG="🔷"
+
+cd /bots/neptun/
+
+OLD_COMMIT=$(git rev-parse HEAD)
+
+echo "$EG update the source"
+git pull
+echo $SPACER
+
+NEW_COMMIT=$(git rev-parse HEAD)
+
+function check_diff {
+    local file_has_changed=$(git diff --name-only $OLD_COMMIT...$NEW_COMMIT --exit-code $1)
+    if [ -z "$file_has_changed" ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+if check_diff "neptun.service"; then
+    echo "$EG reload the service"
+    cp neptun.service /etc/systemd/system/ --force
+    systemctl daemon-reload
+    echo $SPACER
+fi
+
+cargo build
+
+systemctl restart neptun
+cat $(systemctl status neptun)
+
+echo "Deploy is Done! ✅"
+
