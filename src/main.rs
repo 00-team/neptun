ee2e5a3a8650d38a24e53b99d97ed06095a1b3e3
007 <dr007cc@gmail.com>
2024-01-28 20:33:11 +0330

    fix: update

--- src/main.rs
+++ src/main.rs
@@ -1,135 +1,111 @@
-/*
+use std::error::Error;
+use teloxide::dispatching::dialogue::serializer::Json;
+use teloxide::dispatching::{HandlerExt, UpdateFilterExt};
 use teloxide::prelude::*;
 
-#[tokio::main]
-async fn main() {
-    dotenvy::from_path("./secrets.env").unwrap();
-    pretty_env_logger::init();
-    log::info!("Starting throw dice bot...");
-
-    let bot = Bot::from_env();
-
-
-    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_message(msg.chat.id, "hello from neptun! 🌩").await?;
-        Ok(())
-    })
-    .await;
-}
-*/
-
-use rand::distributions::Alphanumeric;
-use rand::{thread_rng, Rng};
-use serde::{Deserialize, Serialize};
-use sqlx::encode::IsNull;
-use sqlx::sqlite::SqliteTypeInfo;
-use sqlx::{Encode, Type};
-use sqlx::{Sqlite, SqlitePool};
-use std::convert::From;
+// use rand::distributions::Alphanumeric;
+// use rand::{thread_rng, Rng};
+use sqlx::SqlitePool;
+// use std::convert::From;
 use std::env;
-use std::string::String;
-use std::time::{SystemTime, UNIX_EPOCH};
-use std::vec::Vec;
+// use std::string::String;
+// use std::time::{SystemTime, UNIX_EPOCH};
 
-#[derive(Clone, PartialEq, Eq, Debug, Default, Serialize, Deserialize, sqlx::FromRow)]
-struct Messages {
-    ids: Vec<i64>,
-}
+mod models;
+// use models::Record;
 
-#[derive(Debug, Default, Serialize, Deserialize, sqlx::FromRow)]
-struct Record {
-    id: i64,
-    slug: String,
-    created_at: i64,
-    messages: Messages,
-    done: bool,
-    count: i64,
-}
+use teloxide::dispatching::dialogue::{SqliteStorage, ErasedStorage};
 
-// impl<'r> Decode<'r, Sqlite> for Messages
-// where
-//     &'r str: Decode<'r, Sqlite>,
-// {
-//     fn decode(
-//         // value: <DB as sqlx::database::HasValueRef<'r>>::ValueRef,
-//         value: SqliteValueRef
-//     ) -> Result<Self, sqlx::error::BoxDynError> {
-//         Ok(Self {
-//             ids: vec![4, 4, 44],
-//         })
-//     }
-// }
+type AddRecordDialogue = Dialogue<AddRecordState, ErasedStorage<AddRecordState>>;
+type HandlerResult = Result<(), Box<dyn Error + Send + Sync>>;
 
-impl<'q> Encode<'q, Sqlite> for Messages {
-    fn encode_by_ref(
-        &self,
-        buf: &mut <Sqlite as sqlx::database::HasArguments<'q>>::ArgumentBuffer,
-    ) -> IsNull {
-        buf.push(sqlx::sqlite::SqliteArgumentValue::Text(
-            std::borrow::Cow::Owned("[3, 4]".to_owned()),
-        ));
-
-        IsNull::No
-    }
-}
-
-impl Type<Sqlite> for Messages {
-    fn type_info() -> SqliteTypeInfo {
-        <&str as Type<Sqlite>>::type_info()
-    }
-}
-
-impl From<String> for Messages {
-    fn from(value: String) -> Self {
-        Self { ids: vec![8, 0] }
-    }
+#[derive(Clone, Default, serde::Serialize, serde::Deserialize)]
+pub enum AddRecordState {
+    #[default]
+    Start,
+    Add,
+    End,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::from_path("./secrets.env").unwrap();
-    let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
+    pretty_env_logger::init();
 
+    let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
     sqlx::migrate!().run(&pool).await?;
 
-    let mut record = Record::default();
+    let bot = Bot::from_env();
+    let storage = SqliteStorage::open(&env::var("TELOXIDE_STORAGE")?, Json).await?;
 
-    record.created_at = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs() as i64;
-    record.slug = thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(16)
-        .map(char::from)
-        .collect();
-
-    record.messages.ids.push(10);
-    record.messages.ids.push(12);
-
-    let result = sqlx::query_as!(
-        Record,
-        "insert into records (slug, created_at, messages) values(?, ?, ?)",
-        record.slug,
-        record.created_at,
-        record.messages
+    Dispatcher::builder(
+        bot,
+        Update::filter_message()
+            .enter_dialogue::<Message, ErasedStorage<AddRecordState>, AddRecordState>()
+            .branch(dptree::case![AddRecordState::Start].endpoint(start)), // .branch(dptree::case![AddRecordState::Add].endpoint(add))
+                                                                           // .branch(dptree::case![AddRecordState::End].endpoint(end)),
     )
-    .execute(&pool)
-    .await?;
+    .dependencies(dptree::deps![storage])
+    .build()
+    .dispatch()
+    .await;
 
-    record.id = result.last_insert_rowid();
+    // return Ok(());
 
-    println!("{:?}", record);
+    // teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+    //     bot.send_message(msg.chat.id, "hello from neptun! 🌩")
+    //         .await?;
+    //     Ok(())
+    // })
+    // .await;
+
+    // let mut record = Record::default();
+
+    // record.created_at = SystemTime::now()
+    //     .duration_since(UNIX_EPOCH)
+    //     .unwrap()
+    //     .as_secs() as i64;
+    //
+    // record.slug = thread_rng()
+    //     .sample_iter(&Alphanumeric)
+    //     .take(16)
+    //     .map(char::from)
+    //     .collect();
+    //
+    // record.messages.ids.push(10);
+    // record.messages.ids.push(12);
+
+    // let result = sqlx::query_as!(
+    //     Record,
+    //     "insert into records (slug, created_at, messages) values(?, ?, ?)",
+    //     record.slug,
+    //     record.created_at,
+    //     record.messages
+    // )
+    // .execute(&pool)
+    // .await?;
+    //
+    // record.id = result.last_insert_rowid();
+    //
+    // println!("{:?}", record);
 
     // if let Err(e) = user.insert(&pool).await {
     //     println!("{:?}", e);
     // }
 
-    let new_record = sqlx::query_as!(Record, "select * from records where id = $1", record.id)
-        .fetch_one(&pool)
-        .await?;
+    // let new_record = sqlx::query_as!(Record, "select * from records where id = $1", 1)
+    //     .fetch_one(&pool)
+    //     .await?;
+    //
+    // println!("{:?}", new_record);
 
-    println!("{:?}", new_record);
+    Ok(())
+}
+
+async fn start(bot: Bot, dialogue: AddRecordDialogue, msg: Message) -> HandlerResult {
+    bot.send_message(msg.chat.id, "hi this is the start!")
+        .await?;
+    dialogue.update(AddRecordState::Add).await?;
 
     Ok(())
 }
---
+++ src/models.rs
@@ -0,0 +1,61 @@
+use serde::{Deserialize, Serialize};
+use sqlx::encode::IsNull;
+use sqlx::sqlite::{SqliteArgumentValue, SqliteTypeInfo};
+use sqlx::Sqlite;
+use sqlx::{Encode, Type};
+use std::borrow::Cow;
+use std::vec::Vec;
+
+#[derive(Clone, PartialEq, Eq, Debug, Default, Serialize, Deserialize, sqlx::FromRow)]
+pub struct Messages {
+    pub ids: Vec<i64>,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, sqlx::FromRow)]
+pub struct Record {
+    pub id: i64,
+    pub slug: String,
+    pub created_at: i64,
+    pub messages: Messages,
+    pub done: bool,
+    pub count: i64,
+}
+
+// impl<'r> Decode<'r, Sqlite> for Messages
+// where
+//     &'r str: Decode<'r, Sqlite>,
+// {
+//     fn decode(
+//         // value: <DB as sqlx::database::HasValueRef<'r>>::ValueRef,
+//         value: SqliteValueRef
+//     ) -> Result<Self, sqlx::error::BoxDynError> {
+//         Ok(Self {
+//             ids: vec![4, 4, 44],
+//         })
+//     }
+// }
+
+impl<'q> Encode<'q, Sqlite> for Messages {
+    fn encode_by_ref(
+        &self,
+        buf: &mut <Sqlite as sqlx::database::HasArguments<'q>>::ArgumentBuffer,
+    ) -> IsNull {
+        let result = serde_json::to_string(self).unwrap_or("{}".to_string());
+        buf.push(SqliteArgumentValue::Text(Cow::Owned(result)));
+
+        IsNull::No
+    }
+}
+
+impl Type<Sqlite> for Messages {
+    fn type_info() -> SqliteTypeInfo {
+        <&str as Type<Sqlite>>::type_info()
+    }
+}
+
+impl From<String> for Messages {
+    fn from(value: String) -> Self {
+        let result: Self = serde_json::from_str(&value).unwrap_or(Self::default());
+        result
+    }
+}
